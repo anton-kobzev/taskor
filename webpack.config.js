4a39d95bb90db851dc31ea2a4324ddfd1384af3d
anton <anton.cobzew@yandex.ru>
2018-08-20 15:11:40 +0300

    Удалены неиспользуемые зависимости

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,6 @@ module.exports = [
                                 sourceMap: true
                             }
                         },
-                        "postcss-loader",
                         {
                             loader: "sass-loader",
                             options: {
